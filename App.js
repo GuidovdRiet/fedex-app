5c1f7c450ecf56e79e4d8c222510afb4b39093ef
Guido van de Riet <guidovdriet@gmail.com>
2018-04-03 12:41:14 +0200

    Add buttons home/not home

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import styled from "styled-components";
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userIsHome: false
+    };
+  }
+
+  userIsHome() {
+    console.log('Home');
+  }
+
+  UserIsNotHome() {
+    console.log("Not Home");
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Test!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Container>
+        <UserIsHome
+          onPress={this.userIsHome}
+          title="Home"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <UserIsNotHome
+          onPress={this.userIsNotHome}
+          title="Not Home"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: red;
+`;
+
+const UserIsHome = styled.Button`
+  background: blue;
+  width: 200px;
+  height: 50px;
+`;
+
+const UserIsNotHome = styled.Button`
+  background: blue;
+  width: 200px;
+  height: 50px;
+`;
