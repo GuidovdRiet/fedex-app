66d713f3c351aca735568da7e5c84c6b2eb6b6d3
Fabian Tjoe-A-On <fabiantjoeaon@gmail.com>
2018-05-07 22:20:05 +0200

    comments todo

--- App.js
+++ App.js
@@ -1,55 +1,59 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import styled from 'styled-components';
 
+// TODO: run BG task die luistert voor socket
+// TODO: Fix notifications (ook permissions van user etc)
+// TODO: On socket event > verstuur notification
+// TODO: Pas nadat dit gebeurd is mag je je informatie updaten
 export default class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      userIsHome: false
-    };
-  }
+        this.state = {
+            userIsHome: false
+        };
+    }
 
-  userIsHome() {
-    console.log("Home");
-  }
+    userIsHome() {
+        console.log('Home');
+    }
 
-  UserIsNotHome() {
-    console.log("Not Home");
-  }
+    userIsNotHome() {
+        console.log('Not Home');
+    }
 
-  render() {
-    return (
-      <Container>
-        <UserIsHomeContainer onPress={this.userIsHome}>
-          <UserIsHomeButton>Home</UserIsHomeButton>
-        </UserIsHomeContainer>
-        <UserIsNotHomeContainer onPress={this.UserIsNotHome}>
-          <UserIsNotHomeButton>Not Home</UserIsNotHomeButton>
-        </UserIsNotHomeContainer>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <UserIsHomeContainer onPress={this.userIsHome}>
+                    <UserIsHomeButton>Home</UserIsHomeButton>
+                </UserIsHomeContainer>
+                <UserIsNotHomeContainer onPress={this.userIsNotHome}>
+                    <UserIsNotHomeButton>Not Home</UserIsNotHomeButton>
+                </UserIsNotHomeContainer>
+            </Container>
+        );
+    }
 }
 
 const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #0000;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #0000;
 `;
 
 const UserIsHomeContainer = styled.TouchableHighlight`
-  background-color: #f7fff3;
-  justify-content: center;
-  align-items: center;
-  height: 150px;
-  width: 90%;
+    background-color: #f7fff3;
+    justify-content: center;
+    align-items: center;
+    height: 150px;
+    width: 90%;
 `;
 
 const UserIsNotHomeContainer = styled(UserIsHomeContainer)`
-  background-color: #fff0f1;
+    background-color: #fff0f1;
 `;
 
 const UserIsHomeButton = styled.Text``;
