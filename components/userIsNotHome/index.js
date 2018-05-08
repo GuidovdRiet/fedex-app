7a8f05a23929a1a2875bce488bca22808e17c5cc
Guido van de Riet <guidovdriet@gmail.com>
2018-05-08 14:39:39 +0200

    Add user not home calendar to make new appointment

--- components/userIsNotHome/index.js
+++ components/userIsNotHome/index.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 class userIsNotHome extends Component {
-    render() {
-        return(
-            <Text>User is not home screen</Text>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      deliveryDate: {}
+    };
+  }
+
+  render() {
+    const date = new Date();
+    const yesterday = date.setDate(date.getDate() - 1);
+
+    return (
+      <Calendar
+        minDate={yesterday}
+        onDayPress={day => {
+          this.setState({ deliveryDate: day });
+          console.log(this.state.deliveryDate);
+        }}
+        markedDates={{
+          [this.state.deliveryDate.dateString]: { selected: true, marked: true, selectedColor: "blue" }
+        }}
+      />
+    );
+  }
 }
 
 export default userIsNotHome;
