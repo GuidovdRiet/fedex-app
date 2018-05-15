ddf4ec535814b07de75a441b63a5c2e5015d7ac1
Guido van de Riet <guidovdriet@gmail.com>
2018-05-15 15:06:57 +0200

    Add tab navigation

--- App.js
+++ App.js
@@ -1,47 +1,58 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import io from 'socket.io-client';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+// import { StackNavigator } from "react-navigation";
+import { TabNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
+import io from "socket.io-client";
 
-import Main from './components/Main';
-import AddNote from './components/UserIsHome';
-import UserIsNotHome from './components/UserIsNotHome';
+import Main from "./components/Main";
+import AddNote from "./components/UserIsHome";
+import UserIsNotHome from "./components/UserIsNotHome";
+import Account from "./components/Account";
 
 // const socketClient = io('http://45.77.159.108:7000');
-const socketClient = io('http://localhost:7000');
+const socketClient = io("http://localhost:7000");
 
 const mapSocketClientToNavigation = Component => {
-    return class extends Component {
-        render() {
-            const { navigation, ...other } = this.props;
-            const {
-                state: { params }
-            } = navigation;
-            return (
-                <Component
-                    {...this.props}
-                    {...params}
-                    socketClient={socketClient}
-                />
-            );
-        }
-    };
+  return class extends Component {
+    render() {
+      const { navigation, ...other } = this.props;
+      const {
+        state: { params }
+      } = navigation;
+      return (
+        <Component {...this.props} {...params} socketClient={socketClient} />
+      );
+    }
+  };
 };
 
-// Om aan te geven of je thuis of niet thuis bent
-// Wel thuis bent -> socket uitsturen uit (true) -> naar volgende scherm
-// Niet thuis bent -> socket uitsturen niet thuis (false)  -> naar volgende scherm
-
-// TODO: Naar socket luisteren delivery:init
-// TODO: Fix notifications (ook permissions van user etc)
-// TODO: On socket event (delivery:init, zie index.js van back-end) > verstuur notification
-// TODO: Pas nadat dit gebeurd is mag je je informatie updaten (socket emitten)
-
-const App = StackNavigator({
-    Home: { screen: mapSocketClientToNavigation(Main) },
-    AddNote: { screen: mapSocketClientToNavigation(AddNote) },
-    UserIsNotHome: { screen: mapSocketClientToNavigation(UserIsNotHome) }
+const App = TabNavigator({
+  Delivery: {
+    screen: mapSocketClientToNavigation(Main),
+    navigationOptions: {
+      tabBarIcon: (
+        <Icon name="package-down" type="material-community" color="#fff" />
+      ),
+      tabBarLabel: "Delivery"
+    }
+  },
+  Account: {
+    screen: mapSocketClientToNavigation(Account),
+    navigationOptions: {
+      tabBarIcon: (
+        <Icon name="account" type="material-community" color="#fff" />
+      ),
+      tabBarLabel: "Account"
+    }
+  }
 });
+
+// const App = StackNavigator({
+//   Home: { screen: mapSocketClientToNavigation(Main) },
+//   AddNote: { screen: mapSocketClientToNavigation(AddNote) },
+//   UserIsNotHome: { screen: mapSocketClientToNavigation(UserIsNotHome) }
+// });
 
 export default () => <App />;
